a83830c133cc22641dbaf37fceffa7bf62c07a9a
Sabina Gahramanova <sabina.ghrmnli@gmail.com>
2023-04-15 21:04:13 -0600

    finished question 3

--- assignment/scripts/array-practice.js
+++ assignment/scripts/array-practice.js
@@ -32,15 +32,17 @@ console.log('--- 3. Accessing items in an array ---');
 console.log('First animal is', animalArray[0])
 
 // 3.a. TODO: Log the second animal in the array 
-
+console.log ('Second animal is', animalArray[1])
 
 // 3.b. TODO: Log the last animal in the array using it's array index 
+console.log ('Second animal is', animalArray[3])
 
 
 // 3.c. (STRETCH) TODO: Log the last animal by using the array length, 
 //      instead of the exact index number of the last item
 
-
+animalArray[animalArray.length -3]
+console.log ('Last animal is' , animalArray[animalArray.length -3])
 
 // 4. Adding & Removing Array Items
 console.log('--- 4. Adding and removing array items ---');
